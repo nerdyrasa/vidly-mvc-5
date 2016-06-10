bdfa325c16f018b4e1fe68a29be46a395359591a
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-06-10 16:22:09 -0500

    Update application name and links and add basic MVC for Customer.

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(new Customer() { Id=1, Name="Tom Jones"});
+        }
+    }
+}
