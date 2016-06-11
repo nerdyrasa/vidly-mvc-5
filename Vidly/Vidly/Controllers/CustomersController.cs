01716ccb533d08b8380445c00bd451135a509c42
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-06-11 06:55:00 -0500

    Get customers from database.

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+                _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            return View(new Customer() { Id=1, Name="Tom Jones"});
+            IEnumerable<Customer> customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
         }
     }
 }
