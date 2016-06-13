b2dc0bc8ec12a15b4d9e17964a67c6bb0d07e5d3
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-06-13 12:20:47 -0500

    Enable attribute routing.

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Enable attribute routing
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
